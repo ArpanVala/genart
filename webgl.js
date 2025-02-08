5932fea60036f3c6fea673624cae4c1b08fa69cb
Arpan Vala <valaarpan05@gmail.com>
2025-02-09 00:20:43 +0530

    diffrent mesh will diffrent color

--- webgl.js
+++ webgl.js
@@ -1,4 +1,6 @@
 const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
 // const { roughness } = require("three/tsl");
 import * as THREE from 'three';
 
@@ -7,14 +9,14 @@ import * as THREE from 'three';
 global.THREE = require("three");
 
 // Include any additional ThreeJS examples below
-require("three/examples/js/controls/OrbitControls");  
+require("three/examples/js/controls/OrbitControls");
 
 const settings = {
   // Make the loop animated
   animate: true,
   // Get a WebGL canvas rather than 2D
   context: "webgl",
-  attribute: {antialias : true}
+  attribute: { antialias: true }
 };
 
 const sketch = ({ context }) => {
@@ -25,32 +27,46 @@ const sketch = ({ context }) => {
   });
 
   // WebGL background color
-  renderer.setClearColor("white", 1);
+  renderer.setClearColor( "lightgrey", 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
-  camera.position.set(0, 0, -4);
-  camera.lookAt(new THREE.Vector3());
+  const camera = new THREE.OrthographicCamera();
 
-  // Setup camera controller
-  const controls = new THREE.OrbitControls(camera, context.canvas);
 
   // Setup your scene
   const scene = new THREE.Scene();
 
   // Setup a geometry
   // const geometry = new THREE.SphereGeometry(1,32, 16);
-  const geometry = new THREE.BoxGeometry(1,1, 1);
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
 
   // Setup a material
-  const material = new THREE.MeshBasicMaterial({
-    color: "red",
-    // wireframe: true
-  });
+  // const material = new THREE.MeshBasicMaterial({
+  //   color: "red",
+  //   // wireframe: true
+  // });
 
+  const palette = random.pick(palettes);
   // Setup a mesh with geometry + material
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  for (let i = 0; i < 40; i++) {
+
+    const mesh = new THREE.Mesh(geometry,  new THREE.MeshBasicMaterial({
+    color: random.pick(palette),
+    // wireframe: true
+  }));
+    mesh.position.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1));
+    mesh.scale.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1)
+    )
+    mesh.scale.multiplyScalar(0.5);
+    scene.add(mesh);
+
+  }
 
   // draw each frame
   return {
@@ -58,18 +74,40 @@ const sketch = ({ context }) => {
     resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight, false);
-      camera.aspect = viewportWidth / viewportHeight;
+      // camera.aspect = viewportWidth / viewportHeight;
+
+      const aspect = viewportWidth / viewportHeight;
+
+      // Ortho zoom
+      const zoom = 2.0;
+
+      // Bounds
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+
+      // Near/Far
+      camera.near = -100;
+      camera.far = 100;
+
+      // Set position & look at world center
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
+
+      // Update the camera
       camera.updateProjectionMatrix();
+
     },
     // Update & render your scene here
     render({ time }) {
-      mesh.rotation.y = time * (0.4);
-      controls.update();
+      // mesh.rotation.y = time * (0.4);
+
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
     unload() {
-      controls.dispose();
+
       renderer.dispose();
     }
   };
