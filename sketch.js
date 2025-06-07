955ad87433417efdc3f14e44ade36661f3911a6d
ArpanVala <valaarpan05@gmail.com>
2025-06-07 21:32:41 +0530

    revision

--- sketch.js
+++ sketch.js
@@ -2,9 +2,10 @@ const canvasSketch = require('canvas-sketch');
 const {lerp}= require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
 const colors = require('nice-color-palettes');
+// const colorbrewer = require('colorbrewer');
 const settings = {
   // dimensions: [2048, 2048 ],
-  // dimensions:[1080,1920]
+  dimensions:[1080,1920]
   // dimensions: [1080,2408],
     //units: 'cm',
   
@@ -17,32 +18,39 @@ const settings = {
 
 const sketch = () => {
   const palette = random.pick(colors);
+  // const palette = colorbrewer.Set3[12];
+
+  //this function will return point son a grid
   const createGrid = () => {
     const points = [];
+    //size
     const count=30;
+    //x cordinate
     for(let i=0;i<count;i++){
+      //y cordinate
       for(let j=0;j<count;j++){
         const u= count <= 1 ? 0.5: i/(count-1);
         const v= count <= 1 ? 0.5: j/(count-1);
-        const radius = Math.abs(random.noise2D(u,v))*0.2;
+        const radius = Math.abs(random.noise2D(u,v))*0.9;
         points.push({
-          colors: random.pick(palette),
+          colors:random.pick(palette),
           positions:[u,v],
           radius,
-          rotation: random.noise2D(u,v)
+          rotation: random.noise2D(u * 0.5, v*0.5)
         });
       }
     }
     return points;
   }
+  //calling the grid function
   // random.setSeed(5);
-  const points = createGrid().filter(()=>{return random.value() > 0.2});
-  const margin = 400;
+  const points = createGrid().filter(()=>{return random.value() > 0.4}); //filter is there for randomness
+  const margin = 30;
 
   return ({ context, width, height }) => {
    context.fillStyle='black';
    context.fillRect(0,0,width,height);
-   
+   //loop through the points and draw them
    points.forEach(data =>{
     const {
       colors,
@@ -64,7 +72,7 @@ const sketch = () => {
     context.font=`${radius * width }px "sans-serif"`;
     context.translate(x, y);
     context.rotate(rotation);
-    context.fillText('/', 0,0);
+    context.fillText('=', 0,0);
     context.restore();
    });
    
--- webgl.js
+++ webgl.js
@@ -15,9 +15,9 @@ require("three/examples/js/controls/OrbitControls");
 const settings = {
   // Make the loop animated
   animate: true,
-  dimensions: [512, 512],
+  // dimensions: [512, 512],
   // dimensions: [1080,2408],
-  // dimensions:[1080,1920],
+  dimensions:[1080,1920],
   fps: 24,
   duration: 4,
   // Get a WebGL canvas rather than 2D
@@ -56,14 +56,18 @@ const sketch = ({ context }) => {
   // Setup a mesh with geometry + material
   for (let i = 0; i < 40; i++) {
 
-    const mesh = new THREE.Mesh(geometry,  new THREE.MeshStandardMaterial({
-    color: random.pick(palette),
-    // wireframe: true
-  }));
-    mesh.position.set(
+    const mesh = new THREE.Mesh(
+      geometry,  
+      new THREE.MeshStandardMaterial({
+        color: random.pick(palette),
+      })
+    );
+    
+  mesh.position.set(
       random.range(-1, 1),
       random.range(-1, 1),
       random.range(-1, 1));
+    
     mesh.scale.set(
       random.range(-1, 1),
       random.range(-1, 1),
@@ -77,11 +81,10 @@ const sketch = ({ context }) => {
   scene.add(new THREE.AmbientLight('hsl(90, 0%, 40%)'));
 
   const light = new THREE.DirectionalLight('white', 1);
-  light.position.set(0, 100, 0);
+  light.position.set(70, 100, 0);
   scene.add(light);
 
-  const easefn= BezierEasing(.67,0,.29,1.01);
-
+  
   // draw each frame
   return {
     // Handle resize events here
@@ -89,12 +92,12 @@ const sketch = ({ context }) => {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight, false);
       // camera.aspect = viewportWidth / viewportHeight;
-
+      
       const aspect = viewportWidth / viewportHeight;
 
       // Ortho zoom
       const zoom = 2.0;
-
+      
       // Bounds
       camera.left = -zoom * aspect;
       camera.right = zoom * aspect;
@@ -104,17 +107,18 @@ const sketch = ({ context }) => {
       // Near/Far
       camera.near = -100;
       camera.far = 100;
-
+      
       // Set position & look at world center
       camera.position.set(zoom, zoom, zoom);
       camera.lookAt(new THREE.Vector3());
 
       // Update the camera
       camera.updateProjectionMatrix();
-
+      
     },
     // Update & render your scene here
     render({playhead }) {
+      const easefn= BezierEasing(.67,0,.29,1.01);
       let t = Math.sin(playhead *Math.PI * 2);
       scene.rotation.y =easefn(t);
 
